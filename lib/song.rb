9611e02590fcedf8df8e8bf5253aa0204bf4b6bf
Alec Reid <alecreid0093@gmail.com>
2019-09-19 19:08:17 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,29 @@
+class Song 
+
+  attr_accessor :name, :artist, :genre 
+  
+  @@count = 0
+  @@genre = []
+  @@artists = []
+  
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@genre << genre
+    @@artists << artist
+  end  
+
+
+
+
+
+
+
+
+
+
+
+
+end
